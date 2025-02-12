c74d434cf87731d814296dcce009ceda7355fb91
Dhinakar-Shanmugam <dhinakarshanmugamofficial@gmail.com>
2025-02-12 02:38:50 -0800

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,14 @@
+public class Conditional {
+    public static void main(String[] args) {
+        int a = 86;
+        int b = 94;
+
+        if(true){
+            System.out.print("a" + "b");
+        }
+        else{
+            System.out.println(a+b);
+        }
+        System.out.print("a"+b);
+    }
+}
