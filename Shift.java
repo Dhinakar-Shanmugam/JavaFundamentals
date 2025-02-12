91abe4e17dadc9317a194f281d8701e7287bcd7e
Dhinakar-Shanmugam <dhinakarshanmugamofficial@gmail.com>
2025-02-11 23:29:18 -0800

    Add files via upload

---
+++ Shift.java
@@ -0,0 +1,9 @@
+public class Shift {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a << 2;
+        System.out.println(b);
+        b = a >> 2;
+        System.out.println(b);
+    }
+}
