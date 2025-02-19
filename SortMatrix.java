7d159cb830845937b5a5dcdf089d8b3761f2642c
Dhinakar-Shanmugam <dhinakarshanmugamofficial@gmail.com>
2025-02-19 08:26:21 -0800

    Update SortMatrix.java

--- SortMatrix.java
+++ SortMatrix.java
@@ -9,14 +9,14 @@ public class SortMatrix {
         int a[] = new int[n*n];
         int i,j;
         System.out.print("Enter the values for matrix: ");
-        for (i = 0; i < arr.length; i++) {
-            for (j = 0; j < arr.length; j++) {
+        for (i = 0; i < arr.length; i++) {    //Getting Values for matrix
+            for (j = 0; j < arr.length; j++) {   
                 arr[i][j] = scan.nextInt();
             }
         }
 
         System.out.println("Before Sorting: ");
-        for (i = 0; i < arr.length; i++) {
+        for (i = 0; i < arr.length; i++) {   //Print Values Before Sorting
             for ( j = 0; j < arr.length; j++) {
                 System.out.print(arr[i][j] + " ");
             }
@@ -25,7 +25,7 @@ public class SortMatrix {
 
         System.out.println();
         int index = 0;
-        for (i = 0; i < arr.length; i++) {
+        for (i = 0; i < arr.length; i++) {    //Add the matrix values in 1D array
             for (j = 0; j < arr.length; j++) {
                 a[index++] = arr[i][j];
             }
@@ -42,14 +42,14 @@ public class SortMatrix {
         }
 
         index = 0;
-        for (i = 0; i < arr.length; i++) {
+        for (i = 0; i < arr.length; i++) {    //Add Sorted values to 2D Matrix
             for (j = 0; j < arr.length; j++) {
                 arr[i][j] = a[index++];
             }
         }
 
         System.out.println("After Sorting: ");
-        for (i = 0; i < arr.length; i++) {
+        for (i = 0; i < arr.length; i++) {    //Print Matrix
             for ( j = 0; j < arr.length; j++) {
                 System.out.print(arr[i][j] + " ");
             }
