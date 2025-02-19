8712f11af812d73d36bc17d48e39e535af8b3ae1
Dhinakar-Shanmugam <dhinakarshanmugamofficial@gmail.com>
2025-02-19 08:21:29 -0800

    Add files via upload

---
+++ SortMatrix.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class SortMatrix {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number of rows and columns: ");
+        int n = scan.nextInt();
+        int arr[][] = new int[n][n];
+        int a[] = new int[n*n];
+        int i,j;
+        System.out.print("Enter the values for matrix: ");
+        for (i = 0; i < arr.length; i++) {
+            for (j = 0; j < arr.length; j++) {
+                arr[i][j] = scan.nextInt();
+            }
+        }
+
+        System.out.println("Before Sorting: ");
+        for (i = 0; i < arr.length; i++) {
+            for ( j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        int index = 0;
+        for (i = 0; i < arr.length; i++) {
+            for (j = 0; j < arr.length; j++) {
+                a[index++] = arr[i][j];
+            }
+        }
+
+        for (i = 0; i < a.length; i++) {   //Bubble Sort
+            for (j = i+1; j < a.length; j++) {
+               if(a[i] > a[j]){
+                   int temp = a[i];
+                   a[i] = a[j];
+                   a[j] = temp;
+               }
+            }
+        }
+
+        index = 0;
+        for (i = 0; i < arr.length; i++) {
+            for (j = 0; j < arr.length; j++) {
+                arr[i][j] = a[index++];
+            }
+        }
+
+        System.out.println("After Sorting: ");
+        for (i = 0; i < arr.length; i++) {
+            for ( j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
